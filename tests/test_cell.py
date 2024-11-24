709701132296b88d893e72d7bedeb900e3eca40e
1633672_ <marc.belmonteal@autonoma.cat>
2024-11-24 20:52:27 +0100

    Afegides funcionalitats set_value i set_invalid_value a Cell

--- tests/test_cell.py
+++ tests/test_cell.py
@@ -1,8 +1,19 @@
 # tests/test_cell.py
-
+import pytest
 from src.model.cell import Cell
 
 def test_initialize_empty_cell():
     cell = Cell()
     assert cell.value == 0  # La celda debe estar vacía al inicio
     assert cell.is_empty()  # Método para verificar que está vacía
+
+def test_set_value():
+    cell = Cell()
+    cell.set_value(2)
+    assert cell.value == 2
+    assert not cell.is_empty()
+
+def test_set_invalid_value():
+    cell = Cell()
+    with pytest.raises(ValueError):
+        cell.set_value(8)  # Debe lanzar una excepción si el valor no es válido
