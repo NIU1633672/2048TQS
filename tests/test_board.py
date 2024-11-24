4782a5329f2d649d1773ef2bda1176bb642eb178
1633672_ <marc.belmonteal@autonoma.cat>
2024-11-25 00:09:02 +0100

    Implementat un test a test_board per a comprovar que el tauler inicial estigui buït

--- tests/test_board.py
+++ tests/test_board.py
@@ -5,3 +5,7 @@ def test_initialize_empty_board():
     board = Board(4)  # Creamos un tablero de 4x4
     expected_board = [[0, 0, 0, 0] for _ in range(4)]
     assert board.grid == expected_board
+
+def test_board_is_empty():
+    board = Board(4)  # Creamos un tablero 4x4
+    assert board.is_empty()  # Al inicio, todas las celdas deberían estar vacías
