776c5140d1d9b702dcff2bf5cc61345e148cd0b6
1633672_ <marc.belmonteal@autonoma.cat>
2024-11-24 20:53:34 +0100

    Afegit codi per resoldre els tests de set value de Cell

--- src/model/cell.py
+++ src/model/cell.py
@@ -1,6 +1,14 @@
+# src/model/cell.py
+
 class Cell:
     def __init__(self):
         self.value = 0
 
     def is_empty(self):
         return self.value == 0
+
+    def set_value(self, value):
+        """Método para asignar un valor a la celda, solo 0, 2 o 4 son válidos."""
+        if value not in [0, 2, 4]:
+            raise ValueError("El valor debe ser 0, 2 o 4")
+        self.value = value
