924db1221391c796c6102bdcf9cf8a83fb62646b
1633672_ <marc.belmonteal@autonoma.cat>
2024-11-30 12:46:06 +0100

    Solucionat errors a game_controller que feien que els tests de moviment , ubicats a test_board.py fallessin

--- src/controller/game_controller.py
+++ src/controller/game_controller.py
@@ -6,6 +6,7 @@ class GameController:
     def __init__(self, size):
         self.game = Game(size)  # Crear el juego
         self.view = GameView()  # Inicializar vista
+        self.board = Board(size)
 
     def add_random_tile(self):
         """Usa el método de Board para agregar una ficha aleatoria."""
@@ -18,6 +19,21 @@ class GameController:
     def display(self):
         """Pide a la vista que muestre el tablero actual."""
         self.view.display_board(self.game.board)
+        
+    def move_left(self):
+        """
+        Realiza un movimiento hacia la izquierda en el tablero.
+        """
+        self.board.move_left()
+
+    def move_right(self):
+        self.board.move_right()
+
+    def move_up(self):
+        self.board.move_up()
+        
+    def move_down(self):
+        self.board.move_down()
 
     def play_turn(self, direction):
         """Realiza un movimiento en la dirección especificada."""
