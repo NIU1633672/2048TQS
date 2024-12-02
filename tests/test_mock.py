d84548a4e55d7eb822519605628e78226ab8300b
Xavier Prats <129038685+TheFuryUsS@users.noreply.github.com>
2024-12-02 09:11:08 +0100

    Delete comentarios antiguos

--- tests/test_mock.py
+++ tests/test_mock.py
@@ -16,11 +16,8 @@ class TestGame(unittest.TestCase):
         mock_view = MockView()
         mock_controller = MockController(mock_board, mock_view)
 
-        # Aquí puedes realizar la prueba del flujo completo.
+        # Prueba del flujo completo.
         mock_controller.play_turn()
-
-        # Agrega aquí tus aserciones para verificar el comportamiento esperado
-        # Por ejemplo, puedes verificar el estado del tablero o el puntaje.
 
     def test_with_mockito(self):
         # Nuevo test usando mockito
